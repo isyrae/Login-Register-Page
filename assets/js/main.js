56942df5ebb63c060f41e0c6ac40638ae965d641
Clean Rose <cleanrosexd@gmail.com>
2025-06-27 13:33:23 +0530

    Fresh start âœ¨

---
+++ assets/js/main.js
@@ -0,0 +1,108 @@
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+
+const passwordAccess = (loginPass, loginEye) =>{
+   const input = document.getElementById(loginPass),
+         iconEye = document.getElementById(loginEye)
+/* 
+╭──────────────────────────────────────────────╮
+│  💻 JavaScript for isyrae Login UI           
+│                                              
+│  👨‍💻 Author     : Rahul Mondal               
+│  🔗 GitHub     : https://github.com/isyrae    
+│  📬 Telegram   : https://t.me/isyrae          
+│  📜 License    : MIT                          
+│                                              
+│  ✨ Script powered with clean code & love 💖  
+╰──────────────────────────────────────────────╯
+*/
+
+   iconEye.addEventListener('click', () =>{
+
+      input.type === 'password' ? input.type = 'text'
+						              : input.type = 'password'
+
+      iconEye.classList.toggle('ri-eye-fill')
+      iconEye.classList.toggle('ri-eye-off-fill')
+   })
+}
+/* 
+╭──────────────────────────────────────────────╮
+│  💻 JavaScript for isyrae Login UI           
+│                                              
+│  👨‍💻 Author     : Rahul Mondal               
+│  🔗 GitHub     : https://github.com/isyrae    
+│  📬 Telegram   : https://t.me/isyrae          
+│  📜 License    : MIT                          
+│                                              
+│  ✨ Script powered with clean code & love 💖  
+╰──────────────────────────────────────────────╯
+*/
+passwordAccess('password','loginPassword')
+
+const passwordRegister = (loginPass, loginEye) =>{
+   const input = document.getElementById(loginPass),
+         iconEye = document.getElementById(loginEye)
+
+   iconEye.addEventListener('click', () =>{
+
+      input.type === 'password' ? input.type = 'text'
+						              : input.type = 'password'
+
+
+      iconEye.classList.toggle('ri-eye-fill')
+      iconEye.classList.toggle('ri-eye-off-fill')
+   })
+}
+/* 
+╭──────────────────────────────────────────────╮
+│  💻 JavaScript for isyrae Login UI           
+│                                              
+│  👨‍💻 Author     : Rahul Mondal               
+│  🔗 GitHub     : https://github.com/isyrae    
+│  📬 Telegram   : https://t.me/isyrae          
+│  📜 License    : MIT                          
+│                                              
+│  ✨ Script powered with clean code & love 💖  
+╰──────────────────────────────────────────────╯
+*/
+passwordRegister('passwordCreate','loginPasswordCreate')
+
+const loginAcessRegister = document.getElementById('loginAccessRegister'),
+      buttonRegister = document.getElementById('loginButtonRegister'),
+      buttonAccess = document.getElementById('loginButtonAccess')
+
+buttonRegister.addEventListener('click', () => {
+   loginAcessRegister.classList.add('active')
+})
+
+buttonAccess.addEventListener('click', () => {
+   loginAcessRegister.classList.remove('active')
+})
+/* 
+╭──────────────────────────────────────────────╮
+│  💻 JavaScript for isyrae Login UI           
+│                                              
+│  👨‍💻 Author     : Rahul Mondal               
+│  🔗 GitHub     : https://github.com/isyrae    
+│  📬 Telegram   : https://t.me/isyrae          
+│  📜 License    : MIT                          
+│                                              
+│  ✨ Script powered with clean code & love 💖  
+╰──────────────────────────────────────────────╯
+*/
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+// 🌸 isyrae Login Script | Author: Rahul Mondal | MIT License
+
